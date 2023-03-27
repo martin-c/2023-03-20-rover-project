338cfcb841b0f67868ca1dbd176f0ae4b71946c6
martin-c <martin.clemons@mac.com>
2023-03-27 12:48:04 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("fwd")
+    images.arrowImage(ArrowNames.North).showImage(0)
 })
 input.onButtonPressed(Button.AB, function () {
     input.calibrateCompass()
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "ping") {
-        basic.showIcon(IconNames.No)
-    }
 })
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Square)
 })
 loops.everyInterval(200, function () {
     radio.sendValue("hdg", input.compassHeading())
